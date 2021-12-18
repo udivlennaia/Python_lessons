8ec68151618f0a42a978147edaa01a2299d41341
udivlennaia <lanakl_76@mail.ru>
2021-12-18 06:44:49 +0300

    первая домашка- мало,но сама

---
+++ n_1.py
@@ -0,0 +1,8 @@
+a = 13
+b = 17
+d = 10
+
+c = a + b + d
+
+print(c)
+print(a,b,d,c)
---
+++ n_3.py
@@ -0,0 +1,7 @@
+n = int(input("введите число n: "))
+a = n * 11
+c = n * 111
+
+x = n + a + c
+print(type (x))
+print("конец программы")
